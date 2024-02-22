c1961bb5d6bed440902baa70d1fcc5ae95b69b15
chunchunmaru <cho227675m@gmail.com>
2024-02-22 19:33:43 +0300

    first commit

---
+++ Worder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PycLan
+{
+    static class Worder
+    {
+        public static Token Wordizator(Token word)
+        {
+            switch (word.View.ToLower())
+            {
+                case "если":
+                    word.Type = TokenType.WORD_IF;
+                    return word;
+                case "иначе":
+                    word.Type = TokenType.WORD_ELSE;
+                    return word;
+                case "пока":
+                    word.Type = TokenType.WHITESPACE;
+                    return word;
+                case "для":
+                    word.Type = TokenType.WORD_FOR;
+                    return word;
+                case "начертать":
+                    word.Type = TokenType.WORD_PRINT;
+                    return word;
+                case "истина":
+                    word.Type = TokenType.WORD_TRUE;
+                    return word;
+                case "правда":
+                    word.Type = TokenType.WORD_TRUE;
+                    return word;
+                case "ложь":
+                    word.Type = TokenType.WORD_FALSE;
+                    return word;
+                default:
+                    return word;
+            }
+        }
+    }
+}
